3f4c452244fbe8140595d277d322ecd810dd85e4
Roice Johnson <rjohn230uncc.edu>
2020-04-05 11:13:51 -0400

    final

--- app.js
+++ app.js
@@ -1,41 +1,42 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const app = express();
+const debug = require('debug')('myapp:server');
+const path = require('path');
+const multer = require('multer');
+const logger = require('morgan');
+const serveIndex = require('serve-index')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './public/images')
+    },
+    filename: (req, file, cb) => {
+        if(path.extname(file.originalname)==".png")
+            cb(null, file.fieldname + '-'+file.originalname)
+        else
+            cb(new Error('Only png are allowed'));
+    }
+});
 
-var app = express();
+//will be using this for uplading
+const upload = multer({ storage: storage }).single('avatar');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+
+app.use(logger('tiny'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static('public'));
+app.use('/ftp', express.static('public'), serveIndex('public', {'icons': true}));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/', function(req,res) {
+    return res.send("hello from my app express server!")
+})
+app.get('/avatar', function(req,res) {
+    res.sendFile(`./public/images.avatar`)
+})
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
+app.post('/upload_profile_picture', function(req,res) {
+    upload(req,res,(err)=> res.send(err));
+})
 module.exports = app;
